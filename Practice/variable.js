77b888baa13c64c4642e323816d95e7af3a65605
ALEEMSaleem <103898060+ALEEMSaleem@users.noreply.github.com>
2023-11-02 20:16:34 +0000

    add array

---
+++ Practice/variable.js
@@ -0,0 +1,19 @@
+console.log('Hello World')
+
+var value;
+value='learning';
+console.log(value)
+
+let stu={
+    name:'david',
+    age:35,
+    work:true
+
+} 
+console.log(typeof(stu.work))
+
+var chef ='cocked'
+{var chef='food'
+
+}
+console.log(chef)
