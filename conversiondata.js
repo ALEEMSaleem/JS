ab8763023b4c85ec7306fce90f4cef2dfaa22b0d
ALEEMSaleem <103898060+ALEEMSaleem@users.noreply.github.com>
2023-11-09 16:24:22 +0000

    convert data

---
+++ conversiondata.js
@@ -0,0 +1,19 @@
+let score = "55abc"
+//console.log(score)
+//console.log(typeof score)
+
+let convertNumber = Number(score)
+
+//console.log(typeof convertNumber)
+//console.log(convertNumber)
+
+let logedIn = 1
+let booleanLogedIn = Boolean(logedIn)
+//console.log(booleanLogedIn)
+//console.log(typeof booleanLogedIn)
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
