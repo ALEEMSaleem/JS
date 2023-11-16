5d5e38bf671a83ee415f0b92043ce1ca084e81bf
ALEEMSaleem <103898060+ALEEMSaleem@users.noreply.github.com>
2023-11-16 21:42:57 +0000

    apply conditions

---
+++ Practice/conditions.js
@@ -0,0 +1,21 @@
+const score = 100
+
+if (score<90) {
+    console.log(`batsman made ${score} runs`);
+    
+}
+else{
+    console.log(`batsman did not made ${score} runs`);
+}
+
+const temprature =70
+
+if (temprature>80) {
+    console.log(`feel like summer`);
+    
+}else if (temprature<50) {
+    console.log(`feel like winter`);
+    
+}else{
+    console.log(`feel like spring`);
+}
