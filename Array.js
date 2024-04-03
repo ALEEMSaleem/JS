5c147bbdd3ab14666177c7de00378a33cb4a17fb
muhammad saleem <sarowerhome@gmail.com>
2024-04-03 14:26:27 -0400

    Add Array, JS, and Variable files

---
+++ Array.js
@@ -0,0 +1,14 @@
+
+student =[15, 30, 40]
+
+console.log(student)
+
+student1 = ['dave','work',8,'hours',true]
+student1.push('chris')
+console.log(student1)
+
+const cars = ['toyota','honda','nissan','mazda','volvo','bmw']
+cars.push('kia','audi')
+console.log(cars.length)
+console.log(cars[2])
+result = student.concat(cars)
---
+++ Variable.js
@@ -0,0 +1,27 @@
+
+x='done',
+y=50,
+z=x+y
+console.log(z)
+
+const stu ={
+name :'mian',
+age: 45,
+work: true,
+}
+console.log(typeof(stu.work))
+
+let value1="aleem"
+{value1="usman"
+
+}
+console.log(value1)
+
+
+const accountID = 11223344;
+let accountEmail = "hello@com";
+let accountPassword = "123456";
+let accountState = "New Jersey";
+
+console.log(typeof(accountEmail));
+console.table({accountID, accountEmail, accountPassword, accountState});
